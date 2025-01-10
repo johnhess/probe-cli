d14b4ca4e7471a8d8a8cc8b0b66c095789a40ffe
John Hess <john@jthess.com>
2025-01-10 15:38:18 -0600

    add one more test

--- internal/experiment/echcheck/handshake_test.go
+++ internal/experiment/echcheck/handshake_test.go
@@ -1,6 +1,7 @@
 package echcheck
 
 import (
+	"crypto/rand"
 	"crypto/tls"
 	"crypto/x509"
 	"fmt"
@@ -27,9 +28,6 @@ func TestNoEchHandshake(t *testing.T) {
 	testPool := x509.NewCertPool()
 	testPool.AddCert(ts.Certificate())
 	tlsConfig := &tls.Config{
-		// This will be used as the inner SNI and we will validate
-		// we get a certificate for this name.  The outer SNI will
-		// be set based on the ECH config.
 		ServerName:         parsed.Hostname(),
 		InsecureSkipVerify: true,
 		RootCAs:            testPool,
@@ -47,3 +45,41 @@ func TestNoEchHandshake(t *testing.T) {
 	}
 
 }
+func TestGREASEyEchHandshake(t *testing.T) {
+	ts := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "success")
+	}))
+	defer ts.Close()
+
+	parsed, err := url.Parse(ts.URL)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	testPool := x509.NewCertPool()
+	testPool.AddCert(ts.Certificate())
+
+	ecl, err := generateGreaseyECHConfigList(rand.Reader, parsed.Hostname())
+	if err != nil {
+		t.Fatal(err)
+	}
+	tlsConfig := &tls.Config{
+		EncryptedClientHelloConfigList: ecl,
+		ServerName:                     parsed.Hostname(),
+		InsecureSkipVerify:             true,
+		RootCAs:                        testPool,
+	}
+
+	fmt.Printf("parsed: %v\n", parsed.Host)
+	result := handshake([]byte{}, false, false, time.Now(), parsed.Host, parsed, model.DiscardLogger, tlsConfig)
+
+	if result.SoError != nil {
+		t.Fatal("did not expect error, got: ", result.SoError)
+	}
+
+	if result.Failure == nil || (*result.Failure != "unknown_failure: tls: server rejected ECH") {
+		t.Fatal("server should have rejected ECH: ", *result.Failure)
+	}
+}
+
+// TODO: Add a test case with Real ECH once the server-side of crypto/tls supports it.
